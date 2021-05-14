88c8aa8845702d33a839b7b7280306084818dc92
jason <guoyuanquan@hotmail.com>
2021-05-14 17:29:34 +0800

    hot-fix v2

--- git-test/src/main/java/com/tianci/git/Main.java
+++ git-test/src/main/java/com/tianci/git/Main.java
@@ -5,7 +5,7 @@ package com.tianci.git;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello git.");
+        System.out.println("hello git fix.");
         System.out.println("hello jason.");
         System.out.println("hello world.");
         System.out.println("hello hot-fix.");
