70d7eed77ab1253bf8140a7a6ab9c8570ce1b023
jason <guoyuanquan@hotmail.com>
2021-05-14 17:41:43 +0800

    master commit 5

--- git-test/src/main/java/com/tianci/git/Main.java
+++ git-test/src/main/java/com/tianci/git/Main.java
@@ -9,5 +9,6 @@ public class Main {
         System.out.println("hello jason.");
         System.out.println("hello world.");
         System.out.println("hello hot-fix.");
+        System.out.println("push test.");
     }
 }
