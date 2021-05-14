4a3c9dceedda35c214ee2166ad84e197e46670af
jason <guoyuanquan@hotmail.com>
2021-05-14 17:28:38 +0800

    hot-fix commit 1

--- git-test/src/main/java/com/tianci/git/Main.java
+++ git-test/src/main/java/com/tianci/git/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("hello git.");
         System.out.println("hello jason.");
         System.out.println("hello world.");
+        System.out.println("hello hot-fix.");
     }
 }
