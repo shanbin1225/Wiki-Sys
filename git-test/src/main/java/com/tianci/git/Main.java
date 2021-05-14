8312475791d7b9cbd435c38457b5da1c45d271ea
jason <guoyuanquan@hotmail.com>
2021-05-14 17:11:05 +0800

    second commit

--- git-test/src/main/java/com/tianci/git/Main.java
+++ git-test/src/main/java/com/tianci/git/Main.java
@@ -1,7 +1,11 @@
 package com.tianci.git;
 
+/**
+ * @author Jason
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello git.");
+        System.out.println("hello jason.");
     }
 }
