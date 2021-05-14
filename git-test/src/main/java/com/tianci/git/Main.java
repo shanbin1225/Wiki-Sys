d34c17281543af0c1b1b55379e68c9bb6984529f
jason <guoyuanquan@hotmail.com>
2021-05-14 17:19:12 +0800

    third commit

--- git-test/src/main/java/com/tianci/git/Main.java
+++ git-test/src/main/java/com/tianci/git/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello git.");
         System.out.println("hello jason.");
+        System.out.println("hello world.");
     }
 }
